a2cb5fb3bd164abbefff90a9ed65cb35b5497078
Urban Willi <urban.willi@stud.fhgr.ch>
2022-05-16 23:28:49 +0200

    CmakeFile worked properly. go to folder ~/mrProj2/build and run $ cmake ../source $ make -> executable is in the build folder

--- cMakefiles/mcp9808Demo.cmake
+++ cMakefiles/mcp9808Demo.cmake
@@ -1,29 +1,23 @@
-cmake_minimum_required(VERSION 3.10.2)
-project(temperature_dev)
+cmake_minimum_required(VERSION 3.10)
+project(mrProj2
+        LANGUAGES CXX
+)
 
 set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 
-# Libraries
-add_subdirectory("${PROJECT_SOURCE_DIR}/../lib/pigpio" ${PROJECT_SOURCE_DIR}/build")
-
 # Executable
-add_executable(${PROJECT_NAME}
-                ${PROJECT_SOURCE_DIR}/source/src/temp_TestingMain.cpp
-                ${PROJECT_SOURCE_DIR}/source/src/mcp9808.cpp
-                ${PROJECT_SOURCE_DIR}/source/src/pigpioI2c.cpp
-                ${PROJECT_SOURCE_DIR}/source/src/raspiPigpio.cpp
-                )
-							
-# threads needed for pigpio to compile correctly
-set(THREADS_PREFER_PTHREAD_FLAG ON)
-find_package(Threads REQUIRED)
+add_executable(temp_dev
+                ${PROJECT_SOURCE_DIR}/src/temp_TestingMain.cpp
+                ${PROJECT_SOURCE_DIR}/src/mcp9808.cpp
+                ${PROJECT_SOURCE_DIR}/src/pigpioI2c.cpp
+                ${PROJECT_SOURCE_DIR}/src/raspiPigpio.cpp
+)
 
-target_link_libraries(${PROJECT_NAME} PUBLIC pigpio
-                                        PRIVATE Threads::Threads
-                                        )
+target_link_libraries(temp_dev
+                        pigpio
+)
 
-target_include_directories(${PROJECT_NAME} PUBLIC
-                                "${PROJECT_BINARY_DIR}"
-                                "${PROJECT_SOURCE_DIR}/../lib/pigpio"
-                                "${PROJECT_SOURCE_DIR}/source/inc" )
+target_include_directories(temp_dev
+                           PUBLIC ${PROJECT_SOURCE_DIR}/inc
+)
