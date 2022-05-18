451ac051f71d8d1d84edbb856b474e1c25fd6709
Urban Willi <urban.willi@stud.fhgr.ch>
2022-05-18 18:12:04 +0200

    Renamed cmake file and added in shellScript variable execName to change --target on cmake command -> build different targets by changing execName variable.

--- cMakefiles/mrProj2_dev.cmake
+++ cMakefiles/mrProj2_dev.cmake
@@ -6,7 +6,7 @@ project(mrProj2
 set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 
-# Executable
+# Temperature sensor development
 add_executable(temp_dev
                 ${PROJECT_SOURCE_DIR}/src/temp_TestingMain.cpp
                 ${PROJECT_SOURCE_DIR}/src/mcp9808.cpp
@@ -19,5 +19,21 @@ target_link_libraries(temp_dev
 )
 
 target_include_directories(temp_dev
+                           PUBLIC ${PROJECT_SOURCE_DIR}/inc
+)
+
+# Gyro & accelerometer sensor development
+add_executable(gyro_dev
+                ${PROJECT_SOURCE_DIR}/src/gyro_TestingMain.cpp
+                ${PROJECT_SOURCE_DIR}/src/lsm6ds33.cpp
+                ${PROJECT_SOURCE_DIR}/src/pigpioI2c.cpp
+                ${PROJECT_SOURCE_DIR}/src/raspiPigpio.cpp
+)
+
+target_link_libraries(gyro_dev
+                        pigpio
+)
+
+target_include_directories(gyro_dev
                            PUBLIC ${PROJECT_SOURCE_DIR}/inc
 )
