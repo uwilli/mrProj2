7137e8a05833a6cf39b764f452fe8c4cdb6a71c1
Urban Willi <urban.willi@stud.fhgr.ch>
2022-05-19 16:51:18 +0200

    Adapted main routine to calibrate and print gyro/acc data 10 times.

--- GpioTests/gyro_TestingMain.cpp
+++ GpioTests/gyro_TestingMain.cpp
@@ -3,13 +3,22 @@
 
 int main()
 {
+    VecXYZ gyro;
+    VecXYZ acc;
+
     PigpioI2c::i2cScanner();
     sleep(1);
     Lsm6ds33 sens;
     sens.initialise();
     sleep(1);
-    int ret = sens.setGyroMaxDPS(612);
-    std::cout << ret << " returnvalue" << std::endl;
+    sens.calibrate();
+
+    for(int i=0; i<10; i++)
+    {
+        sens.getData(gyro, acc);
+        gyro.print();
+        acc.print();
+    }
 
     std::cout << "Reached end of main loop" << std::endl;
     return 0;
